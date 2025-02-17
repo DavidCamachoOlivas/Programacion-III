401b3b9e04984d54d168a6090f53d0a1c9d2e1b7
DavidCamachoOlivas <dac2_23@alu.uabcs.mx>
2025-02-16 21:24:40 -0700

    Act 7.- Implementa el componente JLabel para color del titulo

--- Act4ClaseVentana/src/aplication/Main.java
+++ Act4ClaseVentana/src/aplication/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Ventana ventana = new Ventana("Facebook");
+		Ventana ventana = new Ventana("Login Page");
 		
 
 	}
--- Act4ClaseVentana/src/aplication/Ventana.java
+++ Act4ClaseVentana/src/aplication/Ventana.java
@@ -1,7 +1,10 @@
 package aplication;
 
-import javax.swing.JFrame;
+import java.awt.Color;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Ventana extends JFrame{
 	public Ventana(String title) {
@@ -14,6 +17,33 @@ public class Ventana extends JFrame{
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
+		
+		JPanel login = new JPanel();
+		login.setLocation(0, 0);
+		login.setSize(1000, 700);
+		login.setOpaque(true);
+		login.setBackground(Color.red);
+		login.setVisible(true);
+		login.setLayout(null);
+		
+		this.add(login);
+		this.repaint();
+		
+		
+		JLabel etiqueta1 = new JLabel("Bienvenido");
+		etiqueta1.setSize(200, 50);
+		etiqueta1.setLocation(400, 50);
+		etiqueta1.setBackground(Color.black);
+		etiqueta1.setVisible(true);
+		etiqueta1.setOpaque(true);
+		etiqueta1.setHorizontalAlignment(JLabel.CENTER); 
+		etiqueta1.setVerticalAlignment(JLabel.CENTER);   
+		etiqueta1.setForeground(Color.white); 
+		
+		login.add(etiqueta1);
+
+
+		
 	}
 
 }
