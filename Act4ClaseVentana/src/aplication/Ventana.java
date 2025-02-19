6bf56e0e545c9f724ba5701eaf8a931d0540241f
DavidCamachoOlivas <dac2_23@alu.uabcs.mx>
2025-02-19 13:51:08 -0700

    Act 14.- Diseña un registro con los componentes vistos en clase

--- Act4ClaseVentana/src/aplication/Ventana.java
+++ Act4ClaseVentana/src/aplication/Ventana.java
@@ -15,27 +15,45 @@ public class Ventana extends JFrame{
 		
 		this.setTitle(title);
 		
-		this.setSize(1000,700);
+		this.setSize(1500,700);
 		
 		this.setResizable(true);
+		this.setLayout(null);
+		
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
+		
+		JPanel panelInicio = login();
+        this.add(panelInicio);
+        
+        JPanel registro = registro();
+        this.add(registro);
+        
+        this.setVisible(true);
+		this.repaint();
+		this.revalidate();
+		
+		
+	}
+	public JPanel login() {
+		
 		Font titulos = new Font("Inika", Font.BOLD, 32);
 		Font subtitulos = new Font("Inika", Font.ROMAN_BASELINE, 22);
 		Font texto = new Font("Inika", Font.ROMAN_BASELINE, 16);
 		
+		
 		JPanel login = new JPanel();
 		login.setLocation(0, 0);
-		login.setSize(1000, 700);
+		login.setSize(750, 700);
 		login.setOpaque(true);
 		login.setBackground(Color.black);
 		login.setVisible(true);
 		login.setLayout(null);
 		
 		
-		this.add(login);
-		this.repaint();
+		//this.add(login);
+		//this.repaint();
 		
 		
 		JPanel etiqueta1 = new JPanel();
@@ -78,7 +96,7 @@ public class Ventana extends JFrame{
 		
 		JTextField nombreInp = new JTextField();
 		
-		nombreInp.setBounds(20, 190, 500, 50);
+		nombreInp.setBounds(20, 180, 500, 40);
 		nombreInp.setBackground(Color.white);
 		nombreInp.setForeground(Color.black);
 		nombreInp.setVisible(true);
@@ -102,7 +120,7 @@ public class Ventana extends JFrame{
 		
 		JPasswordField contraInp = new JPasswordField();
 		
-		contraInp.setBounds(20, 320, 500, 50);
+		contraInp.setBounds(20, 310, 500, 40);
 		contraInp.setBackground(Color.white);
 		contraInp.setForeground(Color.black);
 		contraInp.setVisible(true);
@@ -149,7 +167,239 @@ public class Ventana extends JFrame{
 		etiqueta1.add(enter);
 		this.setVisible(true);
 		
+		return login;
+	}
+	public JPanel registro() {
+		Font titulos = new Font("Inika", Font.BOLD, 32);
+		Font subtitulos = new Font("Inika", Font.ROMAN_BASELINE, 22);
+		Font texto = new Font("Inika", Font.ROMAN_BASELINE, 16);
 		
+		
+		JPanel login = new JPanel();
+		login.setLocation(750,0);
+		login.setSize(1000, 700);
+		login.setOpaque(true);
+		login.setBackground(Color.black);
+		login.setVisible(true);
+		login.setLayout(null);
+		
+		
+		//this.add(login);
+		//this.repaint();
+		
+		
+		JPanel etiqueta1 = new JPanel();
+		etiqueta1.setSize(600, 600);
+		etiqueta1.setLocation(50, 30);
+		etiqueta1.setBackground(Color.decode("#2c8444"));
+		etiqueta1.setVisible(true);
+		etiqueta1.setOpaque(true);
+		etiqueta1.setForeground(Color.decode("#bdc2bc")); 
+		etiqueta1.setLayout(null);
+		
+		login.add(etiqueta1);
+		
+		JLabel titulo = new JLabel("Registro");
+		
+		titulo.setSize(500, 50);
+		titulo.setLocation(50,40);
+		titulo.setBackground(Color.decode("#2c8444"));
+		titulo.setHorizontalAlignment(JLabel.CENTER); 
+		titulo.setVerticalAlignment(JLabel.CENTER); 
+		titulo.setVisible(true);
+		titulo.setOpaque(true);
+		titulo.setForeground(Color.WHITE);
+		titulo.setFont(titulos);
+		
+		etiqueta1.add(titulo);
+
+		JLabel nombre = new JLabel("Nombre de usuario:");
+		
+		nombre.setSize(500, 50);
+		nombre.setLocation(20,100);
+		nombre.setBackground(Color.decode("#2c8444"));
+		nombre.setHorizontalAlignment(JLabel.LEFT); 
+		nombre.setVerticalAlignment(JLabel.CENTER); 
+		nombre.setVisible(true);
+		nombre.setOpaque(true);
+		nombre.setForeground(Color.WHITE);
+		nombre.setFont(subtitulos);
+		etiqueta1.add(nombre);
+		
+		JTextField nombreInp = new JTextField();
+		
+		nombreInp.setBounds(20, 150, 500, 40);
+		nombreInp.setBackground(Color.white);
+		nombreInp.setForeground(Color.black);
+		nombreInp.setVisible(true);
+		nombreInp.setOpaque(true);
+		nombreInp.setFont(subtitulos);
+		nombreInp.setBorder(BorderFactory.createLineBorder(Color.gray, 2));
+		etiqueta1.add(nombreInp);
+		
+		JLabel biografia = new JLabel("Biografía:");
+		
+		biografia.setSize(150, 50);
+		biografia.setLocation(20,190);
+		biografia.setBackground(Color.decode("#2c8444"));
+		biografia.setHorizontalAlignment(JLabel.LEFT); 
+		biografia.setVerticalAlignment(JLabel.CENTER); 
+		biografia.setVisible(true);
+		biografia.setOpaque(true);
+		biografia.setForeground(Color.WHITE);
+		biografia.setFont(subtitulos);
+		etiqueta1.add(biografia);
+		
+		JTextField bioInp = new JTextField();
+		
+		bioInp.setBounds(20, 240, 380, 100);
+		bioInp.setBackground(Color.white);
+		bioInp.setForeground(Color.black);
+		bioInp.setVisible(true);
+		bioInp.setOpaque(true);
+		bioInp.setFont(subtitulos);
+		bioInp.setBorder(BorderFactory.createLineBorder(Color.gray, 2));
+		etiqueta1.add(bioInp);
+		
+		JLabel lugar = new JLabel("Lugar:");
+		
+		lugar.setSize(150, 50);
+		lugar.setLocation(410,190);
+		lugar.setBackground(Color.decode("#2c8444"));
+		lugar.setHorizontalAlignment(JLabel.LEFT); 
+		lugar.setVerticalAlignment(JLabel.CENTER); 
+		lugar.setVisible(true);
+		lugar.setOpaque(true);
+		lugar.setForeground(Color.WHITE);
+		lugar.setFont(subtitulos);
+		etiqueta1.add(lugar);
+		
+		String [] colonias_dataset = {"Pedregal","Centro","Villas del encanto","Diana Laura"};
+		
+		JComboBox lugarInp = new JComboBox(colonias_dataset);
+		
+		lugarInp.setSize(170, 30);
+		lugarInp.setLocation(410,240);
+		lugarInp.setBackground(Color.white);
+		/*lugar.setHorizontalAlignment(JLabel.LEFT); 
+		lugar.setVerticalAlignment(JLabel.CENTER); */
+		lugarInp.setVisible(true);
+		lugarInp.setOpaque(true);
+		lugarInp.setForeground(Color.black);
+		lugarInp.setFont(texto);
+		etiqueta1.add(lugarInp); 
+		
+		
+		JLabel pref = new JLabel("Preferencias:");
+		
+		pref.setSize(500, 50);
+		pref.setLocation(20,340);
+		pref.setBackground(Color.decode("#2c8444"));
+		pref.setHorizontalAlignment(JLabel.LEFT); 
+		pref.setVerticalAlignment(JLabel.CENTER); 
+		pref.setVisible(true);
+		pref.setOpaque(true);
+		pref.setForeground(Color.WHITE);
+		pref.setFont(subtitulos);
+		etiqueta1.add(pref);
+		
+		JCheckBox caja1 = new JCheckBox();
+		
+		caja1.setBounds(20, 390, 170, 30);
+		caja1.setText("Dulce");
+		caja1.setVisible(true);
+		caja1.setOpaque(true);
+		caja1.setBackground(Color.decode("#2c8444"));
+		caja1.setForeground(Color.white);
+		caja1.setFont(texto);
+		etiqueta1.add(caja1);
+		
+		JCheckBox caja2 = new JCheckBox();
+		
+		caja2.setBounds(190, 390, 170, 30);
+		caja2.setText("Salado");
+		caja2.setVisible(true);
+		caja2.setOpaque(true);
+		caja2.setBackground(Color.decode("#2c8444"));
+		caja2.setForeground(Color.white);
+		caja2.setFont(texto);
+		etiqueta1.add(caja2);
+		
+		JCheckBox caja3 = new JCheckBox();
+		
+		caja3.setBounds(360, 390, 170, 30);
+		caja3.setText("Saludable");
+		caja3.setVisible(true);
+		caja3.setOpaque(true);
+		caja3.setBackground(Color.decode("#2c8444"));
+		caja3.setForeground(Color.white);
+		caja3.setFont(texto);
+		etiqueta1.add(caja3);
+		
+		
+		/*JButton contraOlvidada = new JButton();
+		
+		contraOlvidada.setBounds(280, 370, 260, 30);
+		contraOlvidada.setText("<html><u>¿Olvidaste tu contraseña?</u></html>");
+		contraOlvidada.setVisible(true);
+		contraOlvidada.setOpaque(true);
+		contraOlvidada.setBackground(Color.decode("#2c8444"));
+		contraOlvidada.setForeground(Color.white);
+		contraOlvidada.setBorderPainted(false);
+		contraOlvidada.setFont(texto);
+		etiqueta1.add(contraOlvidada);
+		this.setVisible(true);*/
+		
+		JLabel terminos = new JLabel("Terminos:");
+		
+		terminos.setSize(500, 50);
+		terminos.setLocation(20,420);
+		terminos.setBackground(Color.decode("#2c8444"));
+		terminos.setHorizontalAlignment(JLabel.LEFT); 
+		terminos.setVerticalAlignment(JLabel.CENTER); 
+		terminos.setVisible(true);
+		terminos.setOpaque(true);
+		terminos.setForeground(Color.WHITE);
+		terminos.setFont(subtitulos);
+		etiqueta1.add(terminos);
+		
+		ButtonGroup terminosCheck = new ButtonGroup();
+		
+		JRadioButton terms1 = new JRadioButton("Acepto los terminos", true);
+		terms1.setBounds(20, 470, 170, 30);
+		terms1.setBackground(Color.decode("#2c8444"));
+		terms1.setFont(texto);
+		terms1.setForeground(Color.WHITE);
+		terms1.setBorder(BorderFactory.createLineBorder(Color.red));
+		etiqueta1.add(terms1);
+		
+		JRadioButton terms2 = new JRadioButton("No acepto los terminos");
+		terms2.setBounds(350, 470, 200, 30);
+		terms2.setBackground(Color.decode("#2c8444"));
+		terms2.setFont(texto);
+		terms2.setForeground(Color.WHITE);
+		terms2.setBorderPainted(true);
+		terms2.setBorder(BorderFactory.createLineBorder(Color.red, 3));
+		etiqueta1.add(terms2);
+		
+		terminosCheck.add(terms1);
+		terminosCheck.add(terms2);
+		
+		
+		
+		JButton enter = new JButton();
+		
+		enter.setBounds(400, 532, 160, 40);
+		enter.setText("Ingresar");
+		enter.setVisible(true);
+		enter.setOpaque(true);
+		enter.setBackground(Color.decode("#216233"));
+		enter.setForeground(Color.white);
+		enter.setFont(subtitulos);
+		etiqueta1.add(enter);
+		this.setVisible(true);
+		
+		return login;
 	}
 
 }
