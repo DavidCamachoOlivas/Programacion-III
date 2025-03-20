569a74889f9ebfb8019329a95e3c32b45a7761db
DavidCamachoOlivas <dac2_23@alu.uabcs.mx>
2025-03-20 08:29:20 -0700

    Act 14.- Mejorando el juego (parte 2)

--- Act4ClaseVentana/src/aplication/Tiktactoe.java
+++ Act4ClaseVentana/src/aplication/Tiktactoe.java
@@ -12,6 +12,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import java.awt.Font;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.ImageIcon;
 
 public class Tiktactoe extends JFrame {
 
@@ -29,6 +32,9 @@ public class Tiktactoe extends JFrame {
 	JButton btnNewButton_5;
 	JButton btnNewButton_7;
 	JButton btnNewButton_8;
+	int contadorPlayerX=0,contadorPlayerO=0;
+	JLabel lblNewLabel = new JLabel();
+	JLabel lblNewLabel_1 = new JLabel();
 	
 	
 	
@@ -53,19 +59,21 @@ public class Tiktactoe extends JFrame {
 	 */
 	public Tiktactoe() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 500);
+		setBounds(100, 100, 600, 600);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(92, 92, 92));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(70, 50, 400, 400);
+		panel.setBounds(100, 50, 400, 400);
 		contentPane.add(panel);
-		panel.setLayout(new GridLayout(3, 3, 0, 0));
+		panel.setLayout(new GridLayout(3, 3, 5, 5));
 		
 		btnNewButton_2 = new JButton("");
+		
 		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		panel.add(btnNewButton_2);
 		btnNewButton_2.addActionListener(new ActionListener() {
@@ -76,11 +84,15 @@ public class Tiktactoe extends JFrame {
 				String player = "X";
 				if(turno) {
 					turno=false;
-					player="X";
+					player = "X";
+					btnNewButton_2.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
+					btnNewButton_2.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
 				}
 				else {
 					turno=true;
-					player="O";
+					player = "O";
+					btnNewButton_2.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
+					btnNewButton_2.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
 				}
 				btnNewButton_2.setEnabled(false);
 				btnNewButton_2.setText(player);
@@ -99,11 +111,15 @@ public class Tiktactoe extends JFrame {
 				String player = "X";
 				if(turno) {
 					turno=false;
-					player="X";
+					player = "X";
+					btnNewButton.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
+					btnNewButton.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
 				}
 				else {
 					turno=true;
-					player="O";
+					player = "O";
+					btnNewButton.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
+					btnNewButton.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
 				}
 				btnNewButton.setEnabled(false);
 				btnNewButton.setText(player);
@@ -122,11 +138,15 @@ public class Tiktactoe extends JFrame {
 				String player = "X";
 				if(turno) {
 					turno=false;
-					player="X";
+					player = "X";
+					btnNewButton_3.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
+					btnNewButton_3.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
 				}
 				else {
 					turno=true;
-					player="O";
+					player = "O";
+					btnNewButton_3.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
+					btnNewButton_3.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
 				}
 				btnNewButton_3.setEnabled(false);
 				btnNewButton_3.setText(player);
@@ -145,11 +165,15 @@ public class Tiktactoe extends JFrame {
 				String player = "X";
 				if(turno) {
 					turno=false;
-					player="X";
+					player = "X";
+					btnNewButton_4.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
+					btnNewButton_4.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
 				}
 				else {
 					turno=true;
-					player="O";
+					player = "O";
+					btnNewButton_4.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
+					btnNewButton_4.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
 				}
 				btnNewButton_4.setEnabled(false);
 				btnNewButton_4.setText(player);
@@ -168,11 +192,15 @@ public class Tiktactoe extends JFrame {
 				String player = "X";
 				if(turno) {
 					turno=false;
-					player="X";
+					player = "X";
+					btnNewButton_6.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
+					btnNewButton_6.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
 				}
 				else {
 					turno=true;
-					player="O";
+					player = "O";
+					btnNewButton_6.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
+					btnNewButton_6.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
 				}
 				btnNewButton_6.setEnabled(false);
 				btnNewButton_6.setText(player);
@@ -191,11 +219,15 @@ public class Tiktactoe extends JFrame {
 				String player = "X";
 				if(turno) {
 					turno=false;
-					player="X";
+					player = "X";
+					btnNewButton_1.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
+					btnNewButton_1.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
 				}
 				else {
 					turno=true;
-					player="O";
+					player = "O";
+					btnNewButton_1.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
+					btnNewButton_1.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
 				}
 				btnNewButton_1.setEnabled(false);
 				btnNewButton_1.setText(player);
@@ -214,11 +246,15 @@ public class Tiktactoe extends JFrame {
 				String player = "X";
 				if(turno) {
 					turno=false;
-					player="X";
+					player = "X";
+					btnNewButton_5.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
+					btnNewButton_5.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
 				}
 				else {
 					turno=true;
-					player="O";
+					player = "O";
+					btnNewButton_5.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
+					btnNewButton_5.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
 				}
 				btnNewButton_5.setEnabled(false);
 				btnNewButton_5.setText(player);
@@ -237,11 +273,15 @@ public class Tiktactoe extends JFrame {
 				String player = "X";
 				if(turno) {
 					turno=false;
-					player="X";
+					player = "X";
+					btnNewButton_7.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
+					btnNewButton_7.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
 				}
 				else {
 					turno=true;
-					player="O";
+					player = "O";
+					btnNewButton_7.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
+					btnNewButton_7.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
 				}
 				btnNewButton_7.setEnabled(false);
 				btnNewButton_7.setText(player);
@@ -252,6 +292,7 @@ public class Tiktactoe extends JFrame {
 		btnNewButton_8 = new JButton("");
 		btnNewButton_8.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		panel.add(btnNewButton_8);
+		
 		btnNewButton_8.addActionListener(new ActionListener() {
 
 			
@@ -260,11 +301,15 @@ public class Tiktactoe extends JFrame {
 				String player = "X";
 				if(turno) {
 					turno=false;
-					player="X";
+					player = "X";
+					btnNewButton_8.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
+					btnNewButton_8.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/mikuChileMorron2.jpg")));
 				}
 				else {
 					turno=true;
-					player="O";
+					player = "O";
+					btnNewButton_8.setIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
+					btnNewButton_8.setDisabledIcon(new ImageIcon(Tiktactoe.class.getResource("/aplication/myMelody2.jpg")));
 				}
 				btnNewButton_8.setEnabled(false);
 				btnNewButton_8.setText(player);
@@ -273,10 +318,70 @@ public class Tiktactoe extends JFrame {
 			
 			});
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(100, 0, 400, 500);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		lblNewLabel = new JLabel("Jugador 1: " + contadorPlayerX);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(10, 10, 119, 25);
+		panel_1.add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("Jugador 2: " + contadorPlayerO);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_1.setBounds(271, 10, 119, 25);
+		panel_1.add(lblNewLabel_1);
+		
+		JButton btnNewButton_9 = new JButton("Reiniciar");
+		btnNewButton_9.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton_9.setBounds(130, 459, 142, 31);
+		panel_1.add(btnNewButton_9);
+		
+		btnNewButton_9.addActionListener(new ActionListener() {
+
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnNewButton_2.setEnabled(true);
+				btnNewButton.setEnabled(true);
+				btnNewButton_3.setEnabled(true);
+				btnNewButton_4.setEnabled(true);
+				btnNewButton_6.setEnabled(true);
+				btnNewButton_1.setEnabled(true);
+				btnNewButton_5.setEnabled(true);
+				btnNewButton_7.setEnabled(true);
+				btnNewButton_8.setEnabled(true);
+				
+				btnNewButton_2.setIcon(null);
+				btnNewButton.setIcon(null);
+				btnNewButton_3.setIcon(null);
+				btnNewButton_4.setIcon(null);
+				btnNewButton_6.setIcon(null);
+				btnNewButton_1.setIcon(null);
+				btnNewButton_5.setIcon(null);
+				btnNewButton_7.setIcon(null);
+				btnNewButton_8.setIcon(null);
+				
+				btnNewButton_2.setText("");
+				btnNewButton.setText("");
+				btnNewButton_3.setText("");
+				btnNewButton_4.setText("");
+				btnNewButton_6.setText("");
+				btnNewButton_1.setText("");
+				btnNewButton_5.setText("");
+				btnNewButton_7.setText("");
+				btnNewButton_8.setText("");
+				}
+			
+			});
+		
+		
 		
 		
 	}
 	public void verificar() {
+		
 		if(btnNewButton_2.getText().equals(btnNewButton.getText()) && btnNewButton.getText().equals(btnNewButton_3.getText()) && !btnNewButton_2.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, "Ganó " + btnNewButton_2.getText(), "Ganaste", JOptionPane.INFORMATION_MESSAGE);
 			btnNewButton_2.setEnabled(false);
@@ -288,6 +393,9 @@ public class Tiktactoe extends JFrame {
 			btnNewButton_5.setEnabled(false);
 			btnNewButton_7.setEnabled(false);
 			btnNewButton_8.setEnabled(false);
+			
+			
+			anunciarGanador(btnNewButton_2.getText());
 		}
 		if(btnNewButton_4.getText().equals(btnNewButton_6.getText()) && btnNewButton_6.getText().equals(btnNewButton_1.getText()) && !btnNewButton_4.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, "Ganó " + btnNewButton_4.getText(), "Ganaste", JOptionPane.INFORMATION_MESSAGE);
@@ -300,6 +408,8 @@ public class Tiktactoe extends JFrame {
 			btnNewButton_5.setEnabled(false);
 			btnNewButton_7.setEnabled(false);
 			btnNewButton_8.setEnabled(false);
+			
+			anunciarGanador(btnNewButton_4.getText());
 		}
 		if(btnNewButton_5.getText().equals(btnNewButton_7.getText()) && btnNewButton_7.getText().equals(btnNewButton_8.getText()) && !btnNewButton_5.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, "Ganó " + btnNewButton_5.getText(), "Ganaste", JOptionPane.INFORMATION_MESSAGE);
@@ -312,6 +422,8 @@ public class Tiktactoe extends JFrame {
 			btnNewButton_5.setEnabled(false);
 			btnNewButton_7.setEnabled(false);
 			btnNewButton_8.setEnabled(false);
+			
+			anunciarGanador(btnNewButton_5.getText());
 		}
 		if(btnNewButton_2.getText().equals(btnNewButton_4.getText()) && btnNewButton_4.getText().equals(btnNewButton_5.getText()) && !btnNewButton_2.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, "Ganó " + btnNewButton_2.getText(), "Ganaste", JOptionPane.INFORMATION_MESSAGE);
@@ -324,6 +436,8 @@ public class Tiktactoe extends JFrame {
 			btnNewButton_5.setEnabled(false);
 			btnNewButton_7.setEnabled(false);
 			btnNewButton_8.setEnabled(false);
+			
+			anunciarGanador(btnNewButton_2.getText());
 		}
 		if(btnNewButton.getText().equals(btnNewButton_6.getText()) && btnNewButton_6.getText().equals(btnNewButton_7.getText()) && !btnNewButton.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, "Ganó " + btnNewButton.getText(), "Ganaste", JOptionPane.INFORMATION_MESSAGE);
@@ -336,6 +450,8 @@ public class Tiktactoe extends JFrame {
 			btnNewButton_5.setEnabled(false);
 			btnNewButton_7.setEnabled(false);
 			btnNewButton_8.setEnabled(false);
+			
+			anunciarGanador(btnNewButton.getText());
 		}
 		if(btnNewButton_3.getText().equals(btnNewButton_1.getText()) && btnNewButton_1.getText().equals(btnNewButton_8.getText()) && !btnNewButton_3.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, "Ganó " + btnNewButton_3.getText(), "Ganaste", JOptionPane.INFORMATION_MESSAGE);
@@ -348,6 +464,8 @@ public class Tiktactoe extends JFrame {
 			btnNewButton_5.setEnabled(false);
 			btnNewButton_7.setEnabled(false);
 			btnNewButton_8.setEnabled(false);
+			
+			anunciarGanador(btnNewButton_3.getText());
 		}
 		if(btnNewButton_2.getText().equals(btnNewButton_6.getText()) && btnNewButton_6.getText().equals(btnNewButton_8.getText()) && !btnNewButton_2.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, "Ganó " + btnNewButton_2.getText(), "Ganaste", JOptionPane.INFORMATION_MESSAGE);
@@ -372,8 +490,24 @@ public class Tiktactoe extends JFrame {
 			btnNewButton_5.setEnabled(false);
 			btnNewButton_7.setEnabled(false);
 			btnNewButton_8.setEnabled(false);
+			
+			anunciarGanador(btnNewButton_3.getText());
 		}
 		
 		
+		
+		
+	}
+	private void anunciarGanador(String jugador) {
+		JOptionPane.showMessageDialog(null,"Ganó " + jugador);
+		if(jugador.equals("X")) {
+			contadorPlayerX++;
+		}
+		if(jugador.equals("O")) {
+			contadorPlayerO++;
+		}
+	    lblNewLabel.setText("Jugador 1: " + contadorPlayerX);
+	    lblNewLabel_1.setText("Jugador 2: " + contadorPlayerO);
+
 	}
 }
