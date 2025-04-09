9c35378df5da75c78135fb52696020293d061f39
DavidCamachoOlivas <dac2_23@alu.uabcs.mx>
2025-04-09 01:22:49 -0700

    act 29.- Implementa la actualización de Graphics a través del teclado y los eventos

--- Act4ClaseVentana/src/aplication/keyboard.java
+++ Act4ClaseVentana/src/aplication/keyboard.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -26,10 +27,35 @@ public class keyboard extends JFrame implements KeyListener {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private int x=320, y=250;
+	private int sizePlayer=30;
+	JPanel panel_2;
+	private Player player;
+	private ArrayList<Player> obstaculos = new ArrayList<>();
 
 	/**
 	 * Launch the application.
 	 */
+	
+	class PaintPanel extends JPanel {
+		public PaintPanel(){
+			this.setBackground(Color.black);
+		}
+ 	    @Override
+ 	    protected void paintComponent(Graphics g) {
+ 	        super.paintComponent(g);
+ 	        Graphics g2 = (Graphics2D) g;
+ 	        
+ 	        g2.setColor(Color.green);
+ 	        g2.fillRect(x, y, 30, 30);
+ 	        
+ 	        for (Player obs : obstaculos) {
+ 	        	g2.setColor(obs.c);
+ 	        	g2.fillRect(obs.x, obs.y, obs.w, obs.h);
+ 	       		}
+ 	        
+ 	        }
+ 	    }
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -41,6 +67,7 @@ public class keyboard extends JFrame implements KeyListener {
 				}
 			}
 		});
+		
 	}
 
 	/**
@@ -66,83 +93,116 @@ public class keyboard extends JFrame implements KeyListener {
 		
 		JButton reset = new JButton("Reiniciar");
 		panel_1.add(reset);
-		
-			
-		JPanel panel_2 = new PaintPanel();
-		contentPane.add(panel_2, BorderLayout.CENTER);
-		
-		this.addKeyListener(this);
-		this.setFocusable(true);
-		this.requestFocusInWindow();
-
 		reset.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				x=320;
-				y=250;
+				x=panel_2.getWidth()/2;
+				y=panel_2.getHeight()/2;
+				player.x=x;
+				player.y=y;
 				panel_2.repaint();
 				keyboard.this.requestFocusInWindow();			}
 		});	
+			
+		panel_2 = new PaintPanel();
+		contentPane.add(panel_2, BorderLayout.CENTER);
+		
+		
+		
+		
+		this.addKeyListener(this);
+		this.setFocusable(true);
+		player = new Player(x, y, sizePlayer, sizePlayer, Color.green);
+		obstaculos.add(new Player(200,200,300,50,Color.orange));
+		obstaculos.add(new Player(200,50,50,500,Color.orange));
+		this.requestFocusInWindow();
+
+		
 	}
 	
-	class PaintPanel extends JPanel {
-		public PaintPanel(){
-			this.setBackground(Color.black);
-		}
- 	    @Override
- 	    protected void paintComponent(Graphics g) {
- 	        super.paintComponent(g);
- 	        Graphics g2 = (Graphics2D) g;
- 	        
- 	        g2.setColor(Color.green);
- 	        g2.fillRect(x, y, 30, 30);
- 	        
- 	        }
- 	    }
+	
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
-		if(e.getKeyChar() == 'w') {
-			y-=10;
-		}
-		if(e.getKeyChar() == 's') {
-			y+=10;
-		}
-		if(e.getKeyChar() == 'd') {
-			x+=10;
-		}
-		if(e.getKeyChar() == 'a') {
-			x-=10;
-		}
-		if(x>=640) {
-			x=0;
-		}
-		if(x<=-10) {
-			x=630;
-		}
-		if(y>=550) {
-			y=0;
-		}
-		if(y<=-10) {
-			y=550;
-		}
-		
-		System.out.println("X= "+x+"Y= "+y);
-		
-		this.repaint();
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+		System.out.println("Tecla: " + e.getKeyCode());
+		int playerX=player.x;
+		int playerY=player.y;
+		if(e.getKeyCode() == KeyEvent.VK_W) {
+			playerY-=10;
+			if(player.y - 10 <= 0) {
+				playerY = panel_2.getHeight();
+			}
+		}
+		if(e.getKeyCode() == KeyEvent.VK_S) {
+			playerY+=10;
+			if(playerY > panel_2.getHeight()) {
+				playerY = -sizePlayer;
+			}
+		}
+		if(e.getKeyCode() == KeyEvent.VK_D) {
+			playerX+=10;
+			if(playerX > panel_2.getWidth()) {
+				playerX = -sizePlayer;
+			}
+		}
+		if(e.getKeyCode() == KeyEvent.VK_A) {
+			playerX-=10;
+			if(playerX + sizePlayer < 0) {
+				playerX = panel_2.getWidth();
+			}
+		}
 		
+		
+		Player intento = new Player(playerX, playerY, sizePlayer, sizePlayer, player.c);
+		
+		boolean Colision = false;
+        for (Player obstaculo : obstaculos) {
+            if (intento.colision(obstaculo)) {
+            	System.out.println("Colisión en: " + obstaculo.x + ", " + obstaculo.y );
+            	Colision = true;
+                break;
+            }
+        }
+        if (!Colision) {
+            player.x = playerX;
+            player.y = playerY;
+            x = playerX;
+            y = playerY;
+        }
+		
+		System.out.println("X= "+x+"Y= "+y);
+		this.repaint();
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	class Player{
+		int x,y,h,w;
+		Color c = Color.black;
+		
+		public Player(int x, int y, int h, int w, Color c) {
+			this.x=x;
+			this.y=y;
+			this.h=h;
+			this.w=w;
+			this.c=c;
+		}
+		public Boolean colision(Player target) {
+			return this.x<target.x+target.w &&
+					this.x+this.w>target.x &&
+					this.y<target.y+target.h &&
+					this.y+this.h>target.y;						
+		}
 	}
 
 }
